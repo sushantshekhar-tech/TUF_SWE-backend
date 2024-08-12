02af831b2ce5fc875931c4e21686d8b8a8f0df95
shekh <shekharsushant54@gmail.com>
2024-08-12 23:19:13 +0530

    db env

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "banner_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 
 db.connect((err) => {
   if (err) throw err;
